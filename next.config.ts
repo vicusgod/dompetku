0a6d35b1e570a524253e870f27a8fc85acd0192e
a <a@gmail.com>
2025-12-23 13:49:19 +0700

    chore: add static offline fallback and configure next-pwa

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  fallbacks: {
+    document: "/offline.html",
+  },
 });
 
 const nextConfig: NextConfig = {
