3e4c97b920031d61a31c5e8a14c7eff9fe3d98d1
a <a@gmail.com>
2025-12-24 11:18:11 +0700

    fix: sanitize DATABASE_URL by removing quotes

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DATABASE_URL?.replace(/["']/g, "")!;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });
